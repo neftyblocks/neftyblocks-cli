af78474cbba4f1d9095e15f74df1b44a1fc12d0c
Sato Hori <satohori@Satos-MacBook-Pro.local>
2023-09-05 08:09:33 -0600

    minting fixes

--- src/commands/mint-assets.ts
+++ src/commands/mint-assets.ts
@@ -1,11 +1,18 @@
 import {ux, Command, Flags} from '@oclif/core'
 import assetsService from '../atomichub/asset-service'
 import templateService from '../atomichub/template-service'
+import schemaService from '../atomichub/schema-service'
 const readXlsxFile = require('read-excel-file/node')
 import arrayUtils from '../utils/array-utils'
 import fileUtils from '../utils/file-utils'
 import cryptoUtils from '../utils/crypto-utils'
 import {TransactResult} from 'eosjs/dist/eosjs-api-interfaces'
+
+const typeAliases: any = {
+  image: 'string',
+  ipfs: 'string',
+  bool: 'uint8'
+}
 
 const templateField = 'template'
 const amountField = 'amount'
@@ -20,9 +27,12 @@ export default class MintAssets extends Command {
 
   static flags = {
     file: Flags.string({char: 'f', description: 'Excel file with the templates and amounts', required: true}),
-    batchSize: Flags.integer({char: 's', description: 'Transactions batch size', required: false, default: 100}),
+    batchSize: Flags.integer({char: 't', description: 'Transactions batch size', required: false, default: 100}),
+    collectionName: Flags.string({char: 'c', description: 'Collection name', required: true}),
+    schemaName: Flags.string({char: 's', description: 'Schema name', required: true}),
     ignoreSupply: Flags.boolean({char: 'i', description: 'Ignore supply errors', default: false}),
     password: Flags.string({char: 'k', description: 'CLI password', default: undefined}),
+    addAttributes: Flags.boolean({char: 'a', description: 'Add Attributes', default: false}),
   }
 
   public async run(): Promise<void> {
@@ -30,7 +40,10 @@ export default class MintAssets extends Command {
     const fileTemplate = flags.file
     const batchSize = flags.batchSize
     const ignoreSupply = flags.ignoreSupply
+    const collectionName = flags.collectionName
+    const schemaName = flags.schemaName
     const pwd = flags.password
+    const addAttributes = flags.addAttributes
 
     // validate CLI password
     ux.action.start('Validating...')
@@ -45,6 +58,15 @@ export default class MintAssets extends Command {
     ux.action.stop()
 
     ux.action.start('Getting collection schemas')
+    let schema: any
+    try {
+      schema = await schemaService.getSchema(collectionName, schemaName, config)
+    } catch (error) {
+      this.error(`Error with schema name: '${schemaName}'\n ` + error)
+    }
+    ux.action.stop()
+
+    ux.action.start('Reading xlsx file')
     let sheet = []
     if(fileUtils.fileExists(fileTemplate)){
       try {
@@ -88,9 +110,13 @@ export default class MintAssets extends Command {
       }
       return templateId
     })
+    ux.action.stop()
 
+    ux.action.start('Checking Templates...')
     const templatesMap = await templateService.getTemplatesMap(templateIds, config.atomicUrl)
+    ux.action.stop()
 
+    ux.action.start('Processing data...')
     const mints = []
     for (let row of sheet) {
       const templateId = row[templateIndex]
@@ -100,12 +126,30 @@ export default class MintAssets extends Command {
       if (!template) {
         this.error(`Template ${templateId} doesn't exist`)
       }
-      if (amount <= 0) {
+      if (isNaN(amount) || amount <= 0) {
         this.error('Amount must be greater than 0')
       }
       if (!owner) {
         this.error('Owner is required')
       }
+
+      const attributes: any[] = []
+      if(addAttributes){
+        schema.format.forEach((attr: { name: string; type: string }) => {
+          let value = row[headersMap[attr.name]]
+          if (value !== null && value !== undefined) {
+            const type = typeAliases[attr.type] || attr.type
+            if (attr.type == 'bool') {
+              value = value ? 1 : 0
+            }
+            attributes.push({
+              key: attr.name,
+              value: [type, value],
+            })
+          }
+        })
+      }
+      
 
       // to check if the template has enough max supply we must be mindful of the
       // fact that the same template could be in two different rows, to solve this
@@ -141,7 +185,7 @@ export default class MintAssets extends Command {
           schema_name: template.schema.schema_name,
           template_id: templateId,
           new_asset_owner: owner,
-          immutable_data: [],
+          immutable_data: attributes.length > 0 ? attributes : [],
           mutable_data: [],
           tokens_to_back: [],
         },
@@ -151,12 +195,17 @@ export default class MintAssets extends Command {
     ux.action.stop()
 
     // Create table columns and print table
-    ux.table(mints, {
-      template_id: {get: row => row.actionData.template_id},
-      name: {get: row => row.name},
-      owner: {get: row => row.actionData.new_asset_owner},
-      amount: {get: row => row.amount},
-    })
+    let columns: any = {
+      template_id: {get: (row: any) => row.actionData.template_id},
+      name: {get: (row: any) => row.name},
+      owner: {get: (row: any) => row.actionData.new_asset_owner},
+      amount: {get: (row: any) => row.amount},
+    }
+    for (let attribute of schema.format) {
+      columns[attribute.name] = {get: (row: any) => this.getImmutableValueByKey(attribute.name, row.actionData.immutable_data)}
+    }
+    columns.amount = {get: (row: any) => row.amount}
+    ux.table(mints, columns)
 
     const proceed = await ux.confirm('Continue?')
     if (!proceed) return
@@ -205,4 +254,15 @@ export default class MintAssets extends Command {
     this.exit(0)
     
   }
+
+  getImmutableValueByKey(key: string, immutableData: any) {
+    for (let data of immutableData) {
+      if (data.key === key) {
+        return data.value[1]
+      }
+    }
+    // Return empty string if key isn't defined in immutableData
+    return ''
+  }
+
 }
