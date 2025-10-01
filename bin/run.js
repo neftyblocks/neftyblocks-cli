f94250fdad2af5a73ecbdfd41a162a714948e371
Sato Hori <satohori@Satos-MacBook-Pro-2.local>
2025-09-30 19:23:51 -0600

    Fix for undefined crypto lib

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+import { webcrypto } from 'node:crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
+
 import {execute} from '@oclif/core'
 
 await execute({dir: import.meta.url})
